0d20bffcad4cabc40b17cab0c90606a6eac76606
Volodymyr <fullstack.dev321@gmail.com>
2020-01-05 00:28:41 +0200

    !link

--- _posts/2019-12-30-welcome-to-jekyll.markdown
+++ _posts/2019-12-30-welcome-to-jekyll.markdown
@@ -1,9 +1,9 @@
 ---
 layout: post
-title:  "Welcome to Jekyll!"
+title:  "Welcome to Jekyll"
 description: You’ll find this post in your `_posts` directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run `jekyll serve`, which launches a web server and auto-regenerates your site when a file is updated.
 titles:
-- Welcome to Jekyll!
+- Welcome to Jekyll
 - Code snippets
 - Check out the Jekyll docs
 date:   2019-12-30 08:40:50 +0200
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-$(document).ready(function () {
+$(document).ready(function() {
   // left sidebar switcher
-  $(".choice").click(function () {
+  $(".choice").click(function() {
     var choice = $(this).data("type");
     var target = ".post_" + choice;
     $(".choice").removeClass("active");
@@ -9,47 +9,47 @@ $(document).ready(function () {
     $(target).show();
   });
 
-  $(".post_toggler").click(function () {
+  $(".post_toggler").click(function() {
     $(".post_toggler span").toggle();
     $(".filters").toggle();
     $(".post_all").toggle();
     $(".oal").hide();
   });
 
-  $(".page_toggler").click(function () {
+  $(".page_toggler").click(function() {
     $(".page_toggler span").toggle();
     $(".page_nav").toggle();
   });
-  /*
-    var myElement = document.getElementById('search_box');
-    if (window.addEventListener) {
-      // Normal browsers
-      myElement.addEventListener('DOMSubtreeModified', contentChanged, false);
-    } else
-      if (window.attachEvent) {
-        // IE
-        myElement.attachEvent('DOMSubtreeModified', contentChanged);
-      }
-  
-    function contentChanged() {
-      // this function will run each time the content of the DIV changes
+/*
+  var myElement = document.getElementById('search_box');
+  if (window.addEventListener) {
+    // Normal browsers
+    myElement.addEventListener('DOMSubtreeModified', contentChanged, false);
+  } else
+    if (window.attachEvent) {
+      // IE
+      myElement.attachEvent('DOMSubtreeModified', contentChanged);
     }
-  
-  
-       $(".tipue_search_icon").click(function() {
-        $(".tipue_search_close").toggleClass("hidden");
-        $(".tipue_search_right").toggleClass("hidden");
-        $(".tipue_search_icon").toggleClass("hidden");
-        $(".search_box").toggleClass("invisible");
-      });
-      
-     $(".tipue_search_close").click(function() {
-        $(".tipue_search_close").toggleClass("hidden");
-        $(".tipue_search_right").toggleClass("hidden");
-        $(".tipue_search_icon").toggleClass("hidden");
-        $(".search_box").toggleClass("invisible");
-      });
-  */
+
+  function contentChanged() {
+    // this function will run each time the content of the DIV changes
+  }
+
+
+     $(".tipue_search_icon").click(function() {
+      $(".tipue_search_close").toggleClass("hidden");
+      $(".tipue_search_right").toggleClass("hidden");
+      $(".tipue_search_icon").toggleClass("hidden");
+      $(".search_box").toggleClass("invisible");
+    });
+    
+   $(".tipue_search_close").click(function() {
+      $(".tipue_search_close").toggleClass("hidden");
+      $(".tipue_search_right").toggleClass("hidden");
+      $(".tipue_search_icon").toggleClass("hidden");
+      $(".search_box").toggleClass("invisible");
+    });
+*/
   // right sidebar scroll position indictor
   // Cache selectors
   var lastId,
@@ -59,7 +59,7 @@ $(document).ready(function () {
     // All list items
     (menuItems = rightMenu.find("a")),
     // Anchors corresponding to menu items
-    (scrollItems = menuItems.map(function () {
+    (scrollItems = menuItems.map(function() {
       var item = $($(this).attr("href"));
       if (item.length) {
         return item;
@@ -67,7 +67,7 @@ $(document).ready(function () {
     }));
   // Bind click handler to menu items
   // so we can get a fancy scroll animation
-  menuItems.click(function (e) {
+  menuItems.click(function(e) {
     var href = $(this).attr("href"),
       offsetTop = href === "#" ? 0 : $(href).offset().top - rightMenuHeight + 1;
     $("html, body")
@@ -81,11 +81,11 @@ $(document).ready(function () {
     e.preventDefault();
   });
   // Bind to scroll
-  $(window).scroll(function () {
+  $(window).scroll(function() {
     // Get container scroll position
     var fromTop = $(this).scrollTop() + rightMenuHeight;
     // Get id of current scroll item
-    var cur = scrollItems.map(function () {
+    var cur = scrollItems.map(function() {
       if ($(this).offset().top < fromTop) return this;
     });
     // Get the id of the current element
@@ -95,9 +95,9 @@ $(document).ready(function () {
       lastId = id;
       // Set/remove active class
       menuItems
-        .removeClass("active")
-        .filter("a[href='#" + id + "']")
-        .addClass("active");
+      .removeClass("active")
+      .filter("a[href='#" + id + "']")
+      .addClass("active");
     }
   });
 });
